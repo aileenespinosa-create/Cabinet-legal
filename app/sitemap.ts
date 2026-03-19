e64f1ba3c63029bc5837b5362a47cbd771d22b2c
Aileen ESPINOSA <aileenespinosa@MacBook-Air-3.local>
2026-03-19 14:24:17 -0400

    fix www canonical

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,35 +3,35 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://cabinetlegal.com.do",
+      url: "https://www.cabinetlegal.com.do",
       lastModified: new Date(),
     },
     {
-      url: "https://cabinetlegal.com.do/servicios",
+      url: "https://www.cabinetlegal.com.do/servicios",
       lastModified: new Date(),
     },
     {
-      url: "https://cabinetlegal.com.do/socios",
+      url: "https://www.cabinetlegal.com.do/socios",
       lastModified: new Date(),
     },
     {
-      url: "https://cabinetlegal.com.do/consulta",
+      url: "https://www.cabinetlegal.com.do/consulta",
       lastModified: new Date(),
     },
     {
-      url: "https://cabinetlegal.com.do/blog",
+      url: "https://www.cabinetlegal.com.do/blog",
       lastModified: new Date(),
     },
     {
-      url: "https://cabinetlegal.com.do/blog/como-registrar-una-marca-republica-dominicana",
+      url: "https://www.cabinetlegal.com.do/blog/como-registrar-una-marca-republica-dominicana",
       lastModified: new Date(),
     },
     {
-      url: "https://cabinetlegal.com.do/registro-de-marcas",
+      url: "https://www.cabinetlegal.com.do/registro-de-marcas",
       lastModified: new Date(),
     },
     {
-      url: "https://cabinetlegal.com.do/firma",
+      url: "https://www.cabinetlegal.com.do/firma",
       lastModified: new Date(),
     },
   ];
